2a2638fac20be296bbc73cb3063315286851be78
ajsong <ajsong@MARIOdeMacBook-Pro.local>
2022-08-15 11:47:42 +0800

    init

--- papp/model/WebData.py
+++
@@ -1,8 +0,0 @@
-from .Core import *
-
-
-class _WebData(Core):
-    __name = 'webdata'
-
-
-WebData = _WebData()
--- test.py
+++
@@ -1,7 +0,0 @@
-from papp.tool import *
-from papp.model.WebData import *
-
-
-if __name__ == '__main__':
-    res = WebData.whereId(1).find()
-    print(res)
